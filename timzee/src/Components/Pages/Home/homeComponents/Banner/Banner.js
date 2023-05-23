1f5c9cb88fa71d134ecde11da99353f4a75fb690
LamanHashimova <112489993+LamanHashimova@users.noreply.github.com>
2023-05-23 05:20:25 +0400

    fix Banner

--- timzee/src/Components/Pages/Home/homeComponents/Banner/Banner.js
+++ timzee/src/Components/Pages/Home/homeComponents/Banner/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 import backImageMan from '../../../../../Assets/Images/Untitled-1.webp'
 import backImageWoman from '../../../../../Assets/Images/promo2_c5444c07-50e6-4452-90ed-99ca49a106fc.webp'
 import backImageCouple from '../../../../../Assets/Images/promo3_e10c824c-b077-4e67-be09-c11067066cf5.webp'
-import './Banner.css'
+import './banner.css'
 function Banner() {
    
     return (
